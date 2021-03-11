fb2b9018b4c09f7c142142980048803e369960eb
Miguel Vicente O. Santiago <migscay@users.noreply.github.com>
2021-03-11 11:59:10 +1100

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { useQuery } from 'react-query';
+//styles
+import { Wrapper } from './styles/App.styles';
 
-function App() {
+type BitCoinData = {
+  '15m': number;
+  buy: number;
+  last: number;
+  sell: number;
+  symbol: string;
+}
+
+type Currencies = {
+  [key: string]: BitCoinData; 
+}
+
+const getBCData = async (): Promise<Currencies>  =>
+  await (await fetch('https://blockchain.info/ticker')).json();
+
+const INTERVAL_TIME = 30000; //30 seconds  
+
+const App = () => {
+  const [currency, setCurrency] = useState('USD');
+  const { data, isLoading, error, refetch } = useQuery<Currencies>('bc-data',getBCData);
+  console.log('Refetching data');
+  const handleCurrencySelection = (e: any) => {
+    setCurrency(e.currentTarget.value);
+  }  
+
+  useEffect(() => {
+    const interval = setInterval(refetch,INTERVAL_TIME)
+    return () => clearInterval(interval)
+  }, [refetch])
+
+  if (isLoading) return <div>Loading ... </div>
+  if (error) return <div>Something went horribly wrong ... </div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <>
+        <h2>Bitcoin Price</h2>
+        <select value={currency} onChange={handleCurrencySelection}> 
+          //short circuit, only if data was returned 
+          {data && Object.keys(data).map(currency => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+        <div>
+          <h2>
+            {data && data[currency].symbol}
+            {data && data[currency].last}
+          </h2>
+        </div>
+      </>
+    </Wrapper>
   );
+  
+  
 }
 
 export default App;
---
+++ src/styles/App.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+font-family: Arial, Helvetica, sans-serif;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+height: 100vh;
+overflow: hidden;
+
+div {
+    font-size: 1.5rem;
+    background: lightblue;
+    padding: 20px 40px;
+    border-radius: 40px;
+    margin-top: 40px;
+}
+`;
